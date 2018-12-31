f9b5a45e1933e55692b4b7be69a100645d766ced
Michael Russell <michael.russell@elastic.co>
2018-12-31 16:21:48 +0100

    Set fullname to start with the overridden name
    Before this setting nameOverride would result in resource names like
    "kibana-overrider" which meant that Kibana instances would not be sorted
    nicely with their Elasticsearch cluster. This changes it around so that
    the name will become "overrider-kibana"
    
    Closes #10

--- kibana/tests/kibana_test.py
+++ kibana/tests/kibana_test.py
@@ -151,3 +151,11 @@ updateStrategy:
     assert r['deployment'][name]['spec']['strategy']['type'] == 'RollingUpdate'
     assert r['deployment'][name]['spec']['strategy']['rollingUpdate']['maxUnavailable'] == 1
     assert r['deployment'][name]['spec']['strategy']['rollingUpdate']['maxSurge'] == 1
+
+def test_using_a_name_override():
+    config = '''
+nameOverride: overrider
+'''
+
+    r = helm_template(config)
+    assert 'overrider-kibana' in r['deployment']
