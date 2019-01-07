d21c01c748b4e2a6bec72b95b9a9dbd65912d808
Michael Russell <michael.russell@elastic.co>
2019-01-07 17:29:10 +0100

    Switch Kibana over to goss testing setup

--- helpers/examples.mk
+++ helpers/examples.mk
@@ -1,6 +1,8 @@
 GOSS_VERSION := v0.3.6
 
 goss:
-	kubectl cp test/*.yaml $(GOSS_CONTAINER):/tmp/goss.yaml
-	kubectl exec $(GOSS_CONTAINER) -- sh -c "cd /tmp/ && curl -s -L https://github.com/aelsabbahy/goss/releases/download/$(GOSS_VERSION)/goss-linux-amd64 -o goss && chmod +rx ./goss && ./goss validate --color --format documentation"
+	GOSS_CONTAINER=$$(kubectl get pods -l release=$(RELEASE) -o name | awk -F'/' 'NR==1{ print $$NF }') && \
+	echo Testing with pod: $$GOSS_CONTAINER && \
+	kubectl cp test/*.yaml $$GOSS_CONTAINER:/tmp/goss.yaml && \
+	kubectl exec $$GOSS_CONTAINER -- sh -c "cd /tmp/ && curl -s -L https://github.com/aelsabbahy/goss/releases/download/$(GOSS_VERSION)/goss-linux-amd64 -o goss && chmod +rx ./goss && ./goss validate --color --format documentation"
 
