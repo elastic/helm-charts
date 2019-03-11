1010f447d9aa7ebe74fb193ee102c83e6b735087
Michael Russell <michael.russell@elastic.co>
2019-03-11 21:06:47 +1000

    Retry goss validate every 5 seconds for 30 seconds

--- helpers/examples.mk
+++ helpers/examples.mk
@@ -4,5 +4,5 @@ goss:
 	GOSS_CONTAINER=$$(kubectl get pods -l release=$(RELEASE) -o name | awk -F'/' 'NR==1{ print $$NF }') && \
 	echo Testing with pod: $$GOSS_CONTAINER && \
 	kubectl cp test/*.yaml $$GOSS_CONTAINER:/tmp/goss.yaml && \
-	kubectl exec $$GOSS_CONTAINER -- sh -c "cd /tmp/ && curl -s -L https://github.com/aelsabbahy/goss/releases/download/$(GOSS_VERSION)/goss-linux-amd64 -o goss && chmod +rx ./goss && ./goss validate --color --format documentation"
+	kubectl exec $$GOSS_CONTAINER -- sh -c "cd /tmp/ && curl -s -L https://github.com/aelsabbahy/goss/releases/download/$(GOSS_VERSION)/goss-linux-amd64 -o goss && chmod +rx ./goss && ./goss validate --retry-timeout 30s --sleep 5s --color --format documentation"
 
