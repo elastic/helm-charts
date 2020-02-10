984a7d420de6ee4afcb967e93049093cf4dab5ab
Julien Mailleret <julien.mailleret@elastic.co>
2020-02-10 11:53:41 +0100

    [meta] add phony tasks and help target

--- helpers/common.mk
+++ helpers/common.mk
@@ -2,33 +2,46 @@ default: test
 
 .ONESHELL:
 
-lint:
-	grep 'charts/' ./.helmignore || echo 'charts/' >> ./.helmignore
-	helm lint --strict ./
+.PHONY: help
+help: ## Display this help
+	@awk 'BEGIN {FS = ":.*##"; printf "Usage: make \033[36m<target>\033[0m\n\nTargets:\n"} /^[a-zA-Z_-]+:.*?##/ { printf "  \033[36m%-10s\033[0m %s\n", $$1, $$2 }' $(MAKEFILE_LIST)
 
-template:
-	helm template ./
-
-build:
+.PHONY: build
+build: ## Build helm-tester docker image
 	cd ../helpers/helm-tester && \
 	for i in {1..5}; do docker build -t helm-tester . && break || sleep 15; done
 
-pytest:
-	pytest -sv --color=yes
-
-deps:
+.PHONY: deps
+deps: ## Update helm charts dependencies
 	sed --in-place '/charts\//d' ./.helmignore
 	helm dependency update
 
-test-all: lint deps template pytest
-
-test: build
-	docker run --rm -i --user "$$(id -u):$$(id -g)" -v $$(pwd)/../:/app -w /app/$$(basename $$(pwd)) helm-tester make test-all
-
-helm:
+.PHONY: helm
+helm: ## Deploy helm on k8s cluster
 	kubectl get cs
 	kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default || true
 	helm init --wait --upgrade
 
-lint-python:
+.PHONY: lint
+lint: ## Lint helm templates
+	grep 'charts/' ./.helmignore || echo 'charts/' >> ./.helmignore
+	helm lint --strict ./
+
+.PHONY: lint-python
+lint-python: ## Lint python scripts
 	black --diff --check .
+
+.PHONY: pytest
+pytest: ## Run python tests
+	pytest -sv --color=yes
+
+.PHONY: template
+template: ## Render chart templates
+	helm template ./
+
+.PHONY: test
+test: build ## Run all tests in a docker container
+	docker run --rm -i --user "$$(id -u):$$(id -g)" -v $$(pwd)/../:/app -w /app/$$(basename $$(pwd)) helm-tester make test-all
+
+.PHONY: test-all ## Run all tests
+test-all: lint deps template pytest
