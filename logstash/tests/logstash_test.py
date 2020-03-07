156c07e2fbe948a4cb333cef07b73366de4f22f8
ChiefAlexander <alex.harder@fishtech.group>
2020-03-07 10:22:33 -0600

    Update based on code review

--- logstash/tests/logstash_test.py
+++ logstash/tests/logstash_test.py
@@ -180,6 +180,20 @@ extraContainers: |
     } in extraContainer
 
 
+def test_adding_a_extra_port():
+    config = """
+extraPorts:
+  - name: foo
+    containerPort: 30000
+"""
+    r = helm_template(config)
+    extraPorts = r["statefulset"][name]["spec"]["template"]["spec"]["containers"][0]["ports"]
+    assert {
+        "name": "foo",
+        "containerPort": 30000,
+    } in extraPorts
+
+
 def test_adding_a_extra_init_container():
     config = """
 extraInitContainers: |
