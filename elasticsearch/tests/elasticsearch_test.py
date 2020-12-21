033f2a17617ab70ebbbe13273ff7f0bf2c1df502
Julien Mailleret <8582351+jmlrt@users.noreply.github.com>
2020-12-21 08:20:45 +0100

    [7.10] [all] add hostaliases (#970) (#984)
    This commit allow to define hostAliases in all charts
    
    kubernetes.io/docs/concepts/services-networking/add-entries-to-pod-etc-hosts-with-host-aliases

--- elasticsearch/tests/elasticsearch_test.py
+++ elasticsearch/tests/elasticsearch_test.py
@@ -141,6 +141,7 @@ def test_defaults():
     assert "tolerations" not in r["statefulset"][uname]["spec"]["template"]["spec"]
     assert "nodeSelector" not in r["statefulset"][uname]["spec"]["template"]["spec"]
     assert "ingress" not in r
+    assert "hostAliases" not in r["statefulset"][uname]["spec"]["template"]["spec"]
 
 
 def test_increasing_the_replicas():
@@ -1361,3 +1362,16 @@ fullnameOverride: "customfullName"
         "name": "cluster.initial_master_nodes",
         "value": "customfullName-0," + "customfullName-1," + "customfullName-2,",
     } in env
+
+
+def test_hostaliases():
+    config = """
+hostAliases:
+- ip: "127.0.0.1"
+  hostnames:
+  - "foo.local"
+  - "bar.local"
+"""
+    r = helm_template(config)
+    hostAliases = r["statefulset"][uname]["spec"]["template"]["spec"]["hostAliases"]
+    assert {"ip": "127.0.0.1", "hostnames": ["foo.local", "bar.local"]} in hostAliases
