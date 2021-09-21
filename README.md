ee49f80d28fc99f0fb76f93ca310405adab32775
Julien Mailleret <8582351+jmlrt@users.noreply.github.com>
2021-09-21 11:21:54 +0200

    [meta] add helm 3.6.2 support (#1232) (#1344)
    * [meta] add helm 3.6.0 support
    
    This commit bump the Helm version used in helm-charts tests to 3.6.0.
    
    https://github.com/helm/helm/releases/tag/v3.6.0
    
    * [meta] bump to helm 3.6.2
    
    * [meta] add retry to goss test

--- README.md
+++ README.md
@@ -52,7 +52,7 @@ exact versions are defined under `KUBERNETES_VERSIONS` in
 ### Helm Versions
 
 While we are checking backward compatibility, the charts are only tested with
-Helm version mentioned in [helm-tester Dockerfile][] (currently 3.5.3).
+Helm version mentioned in [helm-tester Dockerfile][] (currently 3.6.2).
 
 
 ## ECK
--- helpers/examples.mk
+++ helpers/examples.mk
@@ -10,7 +10,15 @@ help: ## Display this help
 
 .PHONY: goss
 goss: ## Run goss tests
-	GOSS_CONTAINER=$$(kubectl get --no-headers=true pods -l $(GOSS_SELECTOR) -o custom-columns=:metadata.name --field-selector=status.phase=Running --sort-by=.metadata.creationTimestamp | tail -1 ) && \
-	echo Testing with pod: $$GOSS_CONTAINER && \
-	kubectl cp test/$(GOSS_FILE) $$GOSS_CONTAINER:/tmp/$(GOSS_FILE) && \
-	kubectl exec $$GOSS_CONTAINER -- sh -c "cd /tmp/ && curl -s -L https://github.com/aelsabbahy/goss/releases/download/$(GOSS_VERSION)/goss-linux-amd64 -o goss && chmod +rx ./goss && ./goss --gossfile $(GOSS_FILE) validate --retry-timeout 300s --sleep 5s --color --format documentation"
+	for i in $$(seq 1 5); do \
+		if [ -z "$$GOSS_CONTAINER" ]; then \
+			sleep 5; \
+			echo "Retrieving pod ($$i/5)"; \
+			GOSS_CONTAINER=$$(kubectl get --no-headers=true pods -l "$(GOSS_SELECTOR)" -o custom-columns=:metadata.name --field-selector=status.phase=Running --sort-by=.metadata.creationTimestamp | tail -1 ); \
+		else \
+			echo "Testing with pod: $$GOSS_CONTAINER" && \
+			kubectl cp "test/$(GOSS_FILE)" "$$GOSS_CONTAINER:/tmp/$(GOSS_FILE)" && \
+			kubectl exec "$$GOSS_CONTAINER" -- sh -c "cd /tmp/ && curl -s -L \"https://github.com/aelsabbahy/goss/releases/download/$(GOSS_VERSION)/goss-linux-amd64\" -o goss && chmod +rx ./goss && ./goss --gossfile \"$(GOSS_FILE)\" validate --retry-timeout 300s --sleep 5s --color --format documentation"; \
+			break; \
+		fi; \
+	done
