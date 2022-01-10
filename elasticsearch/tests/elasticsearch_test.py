b25aecd536d82a8d4b0539f2c18773ad67218a46
Christian Kotzbauer <ckotzbauer@samhammer.de>
2022-01-10 11:33:57 +0100

    remove cluster.deprecation_indexing.enabled env when disabled fix #1492

--- elasticsearch/tests/elasticsearch_test.py
+++ elasticsearch/tests/elasticsearch_test.py
@@ -45,7 +45,6 @@ def test_defaults():
         {"name": "discovery.seed_hosts", "value": uname + "-headless"},
         {"name": "network.host", "value": "0.0.0.0"},
         {"name": "cluster.name", "value": clusterName},
-        {"name": "cluster.deprecation_indexing.enabled", "value": "false"},
         {"name": "node.master", "value": "true"},
         {"name": "node.data", "value": "true"},
         {"name": "node.ingest", "value": "true"},
