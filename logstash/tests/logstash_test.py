e96be55a62831d72c373dda82d424b4558a8f57e
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-11-17 00:49:24 +0100

    add test for topologySpreadConstraints

--- logstash/tests/logstash_test.py
+++ logstash/tests/logstash_test.py
@@ -545,6 +545,29 @@ tolerations:
         == "key1"
     )
 
+def test_adding_topology_spread_constraints():
+    config = """
+topologySpreadConstraints:
+  - maxSkew: 1
+    topologyKey: "kubernetes.io/hostname"
+    whenUnsatisfiable: DoNotSchedule
+    matchLabelKeys:
+      - "app"
+"""
+
+    r = helm_template(config)
+    assert (
+        r["statefulset"][name]["spec"]["template"]["spec"]["topologySpreadConstraints"][0]["maxSkew"]
+        == 1
+    )
+    assert (
+        r["statefulset"][name]["spec"]["template"]["spec"]["topologySpreadConstraints"][0]["topologyKey"]
+        == "kubernetes.io/hostname"
+    )
+    assert (
+        r["statefulset"][name]["spec"]["template"]["spec"]["topologySpreadConstraints"][0]["whenUnsatisfiable"][0]
+        == "app"
+    )
 
 def test_adding_pod_annotations():
     config = """
